48ea8f70e1a5a33a929480d7203c68bb1b2133ac
adrianblazejczyk <adrian.blazejczyk@icloud.com>
2023-08-26 19:39:59 +0200

    update

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,23 +1,25 @@
+import styled from 'styled-components';
+
+const StyledListItem = styled.li`
+  color: gray;
+  list-style-type: none;
+`;
+
 const total = (good, neutral, bad) => good + neutral + bad;
 const goodFeedbackPercentage = (good, neutral, bad) =>
   (good / (good + neutral + bad)) * 100;
 
-export const Statistics = ({
-  title,
-  good,
-  neutral,
-  bad,
-  total,
-  goodFeedbackPercentage,
-}) => (
+export const Statistics = ({ title, good, neutral, bad }) => (
   <>
     <h2>{title}</h2>
     <ul>
-      <li>Good: {good}</li>
-      <li>Neutral: {neutral}</li>
-      <li>Bad: {bad}</li>
-      <li>Total: {total}</li>
-      <li>Positive feedback: {goodFeedbackPercentage}% </li>
+      <StyledListItem>Good: {good}</StyledListItem>
+      <StyledListItem>Neutral: {neutral} </StyledListItem>
+      <StyledListItem>Bad: {bad}</StyledListItem>
+      <StyledListItem>Total: {total(good, neutral, bad)}</StyledListItem>
+      <StyledListItem>
+        Positive feedback: {goodFeedbackPercentage(good, neutral, bad)}%{' '}
+      </StyledListItem>
     </ul>
   </>
 );
